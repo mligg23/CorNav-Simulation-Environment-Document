ee9aa0539829b600c3363a4873c49e27b7b21337
Liang Ma <58336073+mligg23@users.noreply.github.com>
2023-08-31 16:44:50 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ release = '0.2.0'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','$READTHEDOCS_OUTPUT']
 
 
 
